0476a96f120423ca982b55edfd08b445e9ef0bcc
Viktoriya-ri <viktoriya23071@gmail.com>
2024-02-17 21:38:12 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SharedLayout from "./pages/SharedLayout";
+
+export const App = () => {
+    return (
+        <div>
+            <SharedLayout />
+        </div>
+    );
+};
